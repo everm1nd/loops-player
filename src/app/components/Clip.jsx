04b3ec22ff6184e1f2118f3dd511c07c411e711a
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-03 20:00:13 +0200

    added basic implementation of sound loop player

--- src/app/components/Clip.jsx
+++ src/app/components/Clip.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Tone from "tone";
 import 'components/Clip/play-icon.svg'
 
-const Clip = () => <button className="button clip" />
+const Clip = ({ url }) => {
+  const player = new Tone.Player(process.env.PUBLIC_URL + url).toMaster();
+  const play = () => { player.start() }
+  return <button className="button clip" onClick={play} />
+}
 
 export default Clip;
--- src/app/components/Track/data.js
+++ src/app/components/Track/data.js
@@ -3,15 +3,15 @@ export default {
     {
       name: "Track 1",
       clips: [
-        { name: "Funky Clip 1"},
-        { name: "Funky Clip 2"}
+        { name: "Funky Clip 1", url: '/media/drum-loop-1.wav' },
+        { name: "Funky Clip 2", url: '/media/drum-loop-1.wav' }
       ]
     },
     {
       name: "Track 2",
       clips: [
-        { name: "Soft Clip 1" },
-        { name: "Soft Clip 2" }
+        { name: "Soft Clip 1", url: '/media/drum-loop-1.wav' },
+        { name: "Soft Clip 2", url: '/media/drum-loop-1.wav' }
       ]
     }
   ]
