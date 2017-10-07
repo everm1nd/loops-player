b6a1b5ed74a1649bbb065cc8de5451d51972bc6e
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-07 17:12:00 +0200

    small refactoring in Clip component: - extracted asset URL fetching in a helper function

--- src/app/components/Clip.jsx
+++ src/app/components/Clip.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Tone from "tone";
 import 'components/Clip/play-icon.svg'
 
+const assetPath = url => process.env.PUBLIC_URL + url;
+
 const Clip = ({ url }) => {
-  const player = new Tone.Player(process.env.PUBLIC_URL + url).toMaster();
+  const player = new Tone.Player(assetPath(url)).toMaster();
   const play = () => { player.start() }
   return <button className="button clip" onClick={play} />
 }
