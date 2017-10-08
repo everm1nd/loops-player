c96d1317ed13ab519c77689696cde06d2a5a419e
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-08 19:13:48 +0200

    renamed App._handleClick to ._handleClipClick to be more explicit about component that we handle

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import 'App.css';
 import Track from "components/Track";
-import tracksData from "components/Track/data";
+import tracksData from "appData";
 import update from 'immutability-helper';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = tracksData
-    this._handleClick = this._handleClick.bind(this)
+    this._handleClipClick = this._handleClipClick.bind(this)
   }
 
-  _handleClick(trackId, clipId) {
+  _handleClipClick(trackId, clipId) {
     const togglePlayback = (playbackState) => playbackState === "started" ? "stopped" : "started"
     const state = update(this.state, {
       tracks: {
@@ -25,7 +25,7 @@ class App extends React.Component {
 
   renderTracks() {
     return this.state.tracks.map( (track, index) =>
-      <Track id={index} key={track.name} {...track} onClick={this._handleClick} />
+      <Track id={index} key={track.name} {...track} onClick={this._handleClipClick} />
     )
   }
 
