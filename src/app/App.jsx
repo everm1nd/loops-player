8e74ba175495dfd738d3bcbadc3183d8e7b1b9ec
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-08 16:57:33 +0200

    toggle clip playback state in App._handleClick

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import 'App.css';
 import Track from "components/Track";
 import tracksData from "components/Track/data";
+import update from 'immutability-helper';
 
-const handleClick = (trackId, clipId) => {
-  console.log(trackId, clipId)
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = tracksData
+    this._handleClick = this._handleClick.bind(this)
+  }
 
-const App = () => {
-  const tracks = tracksData.tracks.map( (track, index) =>
-    <Track id={index} key={track.name} {...track} onClick={handleClick} />
-  )
-  return <div className="app">{tracks}</div>
+  _handleClick(trackId, clipId) {
+    const togglePlayback = (playbackState) => playbackState === "started" ? "stopped" : "started"
+    const state = update(this.state, {
+      tracks: {
+        [trackId]: {
+          clips: { [clipId]: { playbackState: {$apply: togglePlayback} } }
+        }
+      }
+    })
+    this.setState(state)
+  }
+
+  renderTracks() {
+    return this.state.tracks.map( (track, index) =>
+      <Track id={index} key={track.name} {...track} onClick={this._handleClick} />
+    )
+  }
+
+  render() {
+    return <div className="app">{this.renderTracks()}</div>
+  }
 }
 
 export default App;
