79c8a9c954de013884e9c70d7042d8f079165f13
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-08 16:12:57 +0200

    pass track id to Track component changed specs for App to use snapshot

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,8 +4,8 @@ import Track from "components/Track";
 import tracksData from "components/Track/data";
 
 const App = () => {
-  const tracks = tracksData.tracks.map( track =>
-    <Track key={track.name} {...track} />
+  const tracks = tracksData.tracks.map( (track, index) =>
+    <Track id={index} key={track.name} {...track} />
   )
   return <div className="app">{tracks}</div>
 }
