2e5307bfc8933e04357b5fcb544599ef2c6c0df7
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-09 23:39:38 +0200

    modified playback state machine to support onClick and onTick types of state changes

--- src/app/playbackStateMachine.js
+++ src/app/playbackStateMachine.js
@@ -1,15 +1,27 @@
-export default (playbackState) => {
-  // simple playback state machine
-  switch (playbackState) {
-    case "stopped":
-      return "starting"  // start transition
-    case "starting":
-      return "stopped" // cancel transition
-    case "started":
-      return "stopping" // start transition
-    case "stopping":
-      return "started" // cancel transition
-    default:
-      return playbackState
+// simple playback state machine
+export default {
+  onClick(playbackState) {
+    switch (playbackState) {
+      case "stopped":
+        return "starting"  // start transition
+      case "starting":
+        return "stopped" // cancel transition
+      case "started":
+        return "stopping" // start transition
+      case "stopping":
+        return "started" // cancel transition
+      default:
+        return playbackState
+    }
+  },
+  onTick(playbackState) {
+    switch (playbackState) {
+      case "starting":
+        return "started"
+      case "stopping":
+        return "stopped"
+      default:
+        return playbackState
+    }
   }
 }
--- src/test/playbackStateMachine.test.js
+++ src/test/playbackStateMachine.test.js
@@ -1,23 +1,45 @@
 import togglePlayback from 'playbackStateMachine'
 
 describe("playbackStateMachine", () => {
-  describe('transition', () => {
-    it('transition stopped clip to starting', () => {
-      expect(togglePlayback('stopped')).toEqual('starting')
+  describe('onClick', () => {
+    describe('transition', () => {
+      it('transition stopped clip to starting', () => {
+        expect(togglePlayback.onClick('stopped')).toEqual('starting')
+      })
+
+      it('transition started clip to stopping', () => {
+        expect(togglePlayback.onClick('started')).toEqual('stopping')
+      })
     })
 
-    it('transition started clip to stopping', () => {
-      expect(togglePlayback('started')).toEqual('stopping')
-    })
-  })
+    describe('cancel transition', () => {
+      it('transition starting clip to stopped', () => {
+        expect(togglePlayback.onClick('starting')).toEqual('stopped')
+      })
 
-  describe('cancel transition', () => {
-    it('transition starting clip to stopped', () => {
-      expect(togglePlayback('starting')).toEqual('stopped')
+      it('transition stopping clip to started', () => {
+        expect(togglePlayback.onClick('stopping')).toEqual('started')
+      })
     })
 
-    it('transition stopping clip to started', () => {
-      expect(togglePlayback('stopping')).toEqual('started')
-    })
-  })
+    it('returns original state if transition is unknown', () => {
+      expect(togglePlayback.onClick('foo')).toEqual('foo')
+    });
+  });
+
+  describe('onTick', () => {
+    describe('finalize transition of', () => {
+      it('starting clip to started', () => {
+        expect(togglePlayback.onTick('starting')).toEqual('started')
+      });
+
+      it('stopping clip to stopped', () => {
+        expect(togglePlayback.onTick('stopping')).toEqual('stopped')
+      });
+    });
+
+    it('returns original state if transition is unknown', () => {
+      expect(togglePlayback.onTick('foo')).toEqual('foo')
+    });
+  });
 })
