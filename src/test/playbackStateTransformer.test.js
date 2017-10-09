ad4ea05afe649ac58755d96e443a978f532971c1
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-10 00:46:54 +0200

    improved testing of playbackStateTransformer

--- src/test/playbackStateTransformer.test.js
+++ src/test/playbackStateTransformer.test.js
@@ -1,3 +1,50 @@
-it("no worries, we'll test it soooooon :)", () => {
-  expect(true).toBeTrue
+jest.mock('playbackStateMachine', () => (
+  {
+    onTick: jest.fn().mockReturnValue('foo'),
+    onClick: jest.fn().mockReturnValue('bar')
+  }
+))
+
+import { initPlaybackState, clickPlaybackState, tickPlaybackState } from 'playbackStateTransformer'
+
+describe('playbackStateTransformer', () => {
+  const state = {
+    tracks: [
+      { clips: [{}] },
+      { clips: [{}] }
+    ]
+  }
+
+  describe('.initPlaybackState', () => {
+    it("set playbackState of all clips to stopped", () => {
+      expect(initPlaybackState(state)).toEqual({
+        tracks: [
+          { clips: [{ playbackState: "stopped" }] },
+          { clips: [{ playbackState: "stopped" }] }
+        ]
+      })
+    });
+  });
+
+  describe('.tickPlaybackState', () => {
+    it('update state of all clips with value from state machine', () => {
+      expect(tickPlaybackState(state)).toEqual({
+        tracks: [
+          { clips: [{ playbackState: "foo" }] },
+          { clips: [{ playbackState: "foo" }] }
+        ]
+      })
+    });
+  })
+
+  describe('.clickPlaybackState', () => {
+    it('updates state of selected clip with value from state machine', () => {
+      expect(clickPlaybackState(state, { trackId: 0, clipId: 0 })).toEqual({
+        tracks: [
+          { clips: [{ playbackState: "bar" }] },
+          { clips: [{}] }
+        ]
+      })
+    });
+  })
 });
