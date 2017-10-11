e1e64f88a1a4b419b12adcc2e99b40fb3261fa99
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-11 22:19:10 +0200

    added enzyme-to-json and jest-styled-components for nice tests rendering and styled-components testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,8 @@
 // https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-332842582
 import raf from './tempPolyfills'
 
+import 'jest-styled-components'
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
