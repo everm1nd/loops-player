086a2fd4ddaa02a9e95a607d3df321a9a3d79420
Nikita Simakov <everm1nd.mail@gmail.com>
2017-10-12 05:21:47 +0200

    changed clips data and app data

--- src/app/appData.js
+++ src/app/appData.js
@@ -4,17 +4,26 @@ export default {
       name: "Drums",
       color: 'blue',
       clips: [
-        { name: "Funky Clip 1", url: '/media/drums-1.wav', duration: '2m' },
-        { name: "Funky Clip 2", url: '/media/drums-2.wav', duration: '4m' },
-        { name: "Funky Clip 3", url: '/media/drums-1.wav', duration: '2m' }
+        { name: "Electronic Drums", url: '/media/drums-electronic-1-4m.ogg', duration: '4m' },
+        { name: "Acoustic Drums", url: '/media/drums-acoustic-2-4m.ogg', duration: '4m' },
+        { name: "Electronic Drums - Vermona", url: '/media/drums-vermona-3-4m.ogg', duration: '4m' }
       ]
     },
     {
       name: "Melody",
       clips: [
-        { name: "Soft Clip 1", url: '/media/melody-1.wav', duration: '2m' },
-        { name: "Soft Clip 2", url: '/media/melody-2.wav', duration: '2m' },
-        { name: "Soft Clip 3", url: '/media/melody-1.wav', duration: '2m' }
+        { name: "Synth Melody", url: '/media/melody-synth-1-4m.ogg', duration: '2m' },
+        { name: "Guitar Melody", url: '/media/melody-guitar-2-4m.ogg', duration: '2m' },
+        { name: "Synth Pulse Melody", url: '/media/melody-pulse-3-8m.ogg', duration: '2m' }
+      ]
+    },
+    {
+      name: "Bass",
+      color: "Orange",
+      clips: [
+        { name: "Synth Bassline", url: '/media/bass-synth-1-4m.ogg', duration: '4m' },
+        { name: "Guitar Bassline", url: '/media/bass-guitar-2-2m.ogg', duration: '2m' },
+        { name: "Synth Drone Bassline", url: '/media/bass-drone-3-16m.ogg', duration: '16m' }
       ]
     }
   ]
